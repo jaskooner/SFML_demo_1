36cd0031937680d597dfaf39835d61942038a860
scooter <jaskooner.singh@gmail.com>
2018-12-12 12:44:40 +0000

    draw a ball shape on screen

--- SFML_demo_1/main.cpp
+++ SFML_demo_1/main.cpp
@@ -6,9 +6,29 @@ using namespace sf;
 
 // constexpr defines an immutable compile time value
 constexpr int windowWidth(800), windowHeight(600);
+constexpr float ballRadius{10.f};
 
+struct Ball
+{
+    CircleShape shape;
+    
+    // Ball constructor
+    // argument mX -> starting x coord
+    // argument mY -> starting y coord
+    Ball(float mX, float mY)
+    {
+        shape.setPosition(mX, mY);
+        shape.setRadius(ballRadius);
+        shape.setFillColor(Color::Red);
+        shape.setOrigin(ballRadius, ballRadius);
+    }
+};
 
 int main() {
+    
+    // create an instance of Ball
+    // positioned at the center of the window
+    Ball ball{windowWidth / 2, windowHeight / 2};
     
     // creation of the game window
     RenderWindow window{ {windowWidth, windowHeight}, "Arknoid - 1"};
@@ -21,8 +41,9 @@ int main() {
         
         // if 'escape' is pressed, break out of the loop
         if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
-        
-        // show the window contents
+    
+        // render the ball instance on the window
+        window.draw(ball.shape);
         window.display();
         
         // in some OS necesary to call
