575e5a41b0642a1e2afe37e08df9bbc1bdd04bc4
scooter <jaskooner.singh@gmail.com>
2018-12-12 11:42:14 +0000

    Initial Commit

---
+++ SFML_demo_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SFML_demo_1
+//
+//  Created by Jaskooner Singh on 12/12/2018.
+//  Copyright © 2018 Jaskooner Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
